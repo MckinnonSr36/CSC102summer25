840f1d669949d6dec128cb9159f6ffe9bda0c001
MckinnonSr36 <Jarmainemckinnon@gmail.com>
2025-08-04 14:11:03 -0400

    Add files via upload

---
+++ indexcode.js
@@ -0,0 +1,32 @@
+let intervalID = 0;
+
+      function getRandomNumber(){
+        return Math.floor(Math.random() * 1000);
+      }
+
+      function startMove(){
+        let imgMeme = document.getElementById("memeimage");
+
+        setInterval (function (){
+
+          let newTop = getRandomNumber();
+           let newLeft = getRandomNumber();
+
+          imgMeme.style.left =newLeft + "px";
+          imgMeme.style.top =newTop + "px";
+          
+          console.log("left=" + newLeft + "px");
+          console.log("top=" + newTop + "px");
+        },1000 ); 
+
+        document.getElementById("btnStart").disabled = true;
+        document.getElementById("btnStop").disabled = true;
+    }
+
+    function stopMove(){
+
+      clearInterval(intervalID);
+
+       document.getElementById("btnStart").disabled = false;
+        document.getElementById("btnStop").disabled = true;
+    }
