37043d118660c075ccad891e250f061e29b6d294
MckinnonSr36 <Jarmainemckinnon@gmail.com>
2025-08-24 22:44:14 -0400

    Add files via upload

--- indexcode.js
+++ indexcode.js
@@ -1,32 +1,31 @@
 let intervalID = 0;
 
-      function getRandomNumber(){
-        return Math.floor(Math.random() * 1000);
-      }
+function getRandomNumber() {
+  return Math.floor(Math.random() * 1000);
+}
 
-      function startMove(){
-        let imgMeme = document.getElementById("memeimage");
+function startMove() {
+  let imgMeme = document.getElementById("memeimage");
 
-        setInterval (function (){
+  // Assign the interval ID so we can stop it later
+  intervalID = setInterval(function () {
+    let newTop = getRandomNumber();
+    let newLeft = getRandomNumber();
 
-          let newTop = getRandomNumber();
-           let newLeft = getRandomNumber();
+    imgMeme.style.left = newLeft + "px";
+    imgMeme.style.top = newTop + "px";
 
-          imgMeme.style.left =newLeft + "px";
-          imgMeme.style.top =newTop + "px";
-          
-          console.log("left=" + newLeft + "px");
-          console.log("top=" + newTop + "px");
-        },1000 ); 
+    console.log("left=" + newLeft + "px");
+    console.log("top=" + newTop + "px");
+  }, 1000);
 
-        document.getElementById("btnStart").disabled = true;
-        document.getElementById("btnStop").disabled = false;
-    }
+  document.getElementById("btnStart").disabled = true;
+  document.getElementById("btnStop").disabled = false;
+}
 
-    function stopMove(){
+function stopMove() {
+  clearInterval(intervalID);
 
-      clearInterval(intervalID);
-
-       document.getElementById("btnStart").disabled = false;
-        document.getElementById("btnStop").disabled = true;
-    }
+  document.getElementById("btnStart").disabled = false;
+  document.getElementById("btnStop").disabled = true;
+}
